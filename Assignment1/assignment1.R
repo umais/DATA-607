470894270337e620513ba52d4cfcb502195453b9
Umais Siddiqui <umais.siddiqui@aligncare.com>
2017-09-02 00:42:30 -0400

    Some more changes

--- Assignment1/assignment1.R
+++ Assignment1/assignment1.R
@@ -1,13 +1,39 @@
 library(plyr)
 library(RCurl)
-url<-getURL("https://archive.ics.uci.edu/ml/machine-learning-databases/mushroom/agaricus-lepiota.data")
+url<-getURL("https://raw.githubusercontent.com/umais/DATA-607/master/Assignment1/agaricus-lepiota.data")
 
-df <- rename(data.frame(read.csv(text=url,header=FALSE)),c("V1"="Classes","V2"="CapShape","V3"="Title"))
-#d <- rename(df[1:10,c(1:3)],c("V1"="Classes","V2"="CapShape","V3"="Title"))
+df <- rename(data.frame(read.csv(text=url,header=FALSE)),
+             c(
+               "V1"="classes",
+               "V2"="cap_shape",
+               "V3"="cap_surface",
+               "V4"="cap_color",
+               "V5"="Bruises"
+             ))[,c(1:5)]
+
 head(df)
-
-
-
-
-#revalue(myd$classes, c("e" = "edible")) -> myd$classes
-#revalue(myd$classes, c("p" = "poisonous")) -> myd$classes
+revalue(df$classes, c("e" = "edible")) -> df$classes
+revalue(df$classes, c("p" = "poisonous")) -> df$classes
+revalue(df$cap_shape, c("b" = "bell")) -> df$cap_shape
+revalue(df$cap_shape, c("c" = "conical")) -> df$cap_shape
+revalue(df$cap_shape, c("x" = "convex")) -> df$cap_shape
+revalue(df$cap_shape, c("f" = "flat")) -> df$cap_shape
+revalue(df$cap_shape, c("k" = "knobbed")) -> df$cap_shape
+revalue(df$cap_shape, c("s" = "sunken")) -> df$cap_shape
+revalue(df$cap_surface, c("f" = "fibrous")) -> df$cap_surface
+revalue(df$cap_surface, c("g" = "grooves")) -> df$cap_surface
+revalue(df$cap_surface, c("y" = "scaly")) -> df$cap_surface
+revalue(df$cap_surface, c("s" = "smooth")) -> df$cap_surface
+revalue(df$cap_color, c("n" = "Brown")) -> df$cap_color
+revalue(df$cap_color, c("b" = "Buff")) -> df$cap_color
+revalue(df$cap_color, c("c" = "Cinnamon")) -> df$cap_color
+revalue(df$cap_color, c("g" = "Gray")) -> df$cap_color
+revalue(df$cap_color, c("r" = "Green")) -> df$cap_color
+revalue(df$cap_color, c("p" = "Pink")) -> df$cap_color
+revalue(df$cap_color, c("u" = "Purple")) -> df$cap_color
+revalue(df$cap_color, c("e" = "Red")) -> df$cap_color
+revalue(df$cap_color, c("w" = "White")) -> df$cap_color
+revalue(df$cap_color, c("y" = "Yellow")) -> df$cap_color
+revalue(df$Bruises, c("t" = "Yes")) -> df$Bruises
+revalue(df$Bruises, c("f" = "No")) -> df$Bruises
+df
