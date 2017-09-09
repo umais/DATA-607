565ce4139581b16d8aaa6a645f3b6756b428dd0b
Umais Siddiqui <umais20@yahoo.com>
2017-09-09 01:13:07 -0400

    Assignment 2 updates

--- Assignment2/MoviesDatabase.sql
+++ Assignment2/MoviesDatabase.sql
@@ -9,16 +9,17 @@ CREATE TABLE RecentMovie (
   );
   
   CREATE TABLE Person (
-  Id int auto_increment  PRIMARY KEY,
+  PersonId int auto_increment  PRIMARY KEY,
   FirstName varchar(100),
   LastName varchar(100)
 
   );
   
   CREATE TABLE PersonMovieRating (
-  MovieId int primary key,
-  PersonId int primary key,
-  Rating Decimal(2,1)
+  MovieId int ,
+  PersonId int ,
+  Rating Decimal(2,1),
+  primary key(MovieId,PersonId)
   
   );
   
@@ -40,7 +41,7 @@ INSERT INTO Person(FirstName,LastName)VALUES('Stephen','Jackson');
 INSERT INTO Person(FirstName,LastName)VALUES('Saim','Siddiqui');
 
 
-
+-- Ratings for Get Out
 INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(1,1,3);
 INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(1,2,2.5);
 INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(1,3,2);
@@ -48,6 +49,8 @@ INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(1,4,2);
 INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(1,5,2);
 INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(1,6,2);
 INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(1,7,2);
+
+-- Ratings for Logan
 INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(2,1,5);
 INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(2,2,5);
 INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(2,3,3.5);
@@ -56,7 +59,51 @@ INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(2,5,5);
 INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(2,6,5);
 INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(2,7,5);
 
-SELECT m.MovieName,p.FirstName,p.LastName,r.Rating 
+-- Ratings for Spider Man Home Coming
+
+INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(3,1,3.5);
+INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(3,2,4);
+INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(3,3,5);
+INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(3,4,4.5);
+INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(3,5,3);
+INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(3,6,5);
+INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(3,7,5);
+
+
+-- Ratings for Wonder Women
+
+INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(4,1,2.5);
+INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(4,2,3);
+INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(4,3,4);
+INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(4,4,5);
+INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(4,5,4);
+INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(4,6,4);
+INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(4,7,3);
+
+-- Ratings for John Wick 2
+INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(5,1,5);
+INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(5,2,4);
+INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(5,3,3);
+INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(5,4,4);
+INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(5,5,4);
+INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(5,6,4);
+INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(5,7,5);
+
+-- Ratings for Gaurdian of the Galaxy 2
+INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(6,1,5);
+INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(6,2,4);
+INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(6,3,3);
+INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(6,4,4);
+INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(6,5,4);
+INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(6,6,4);
+INSERT INTO PersonMovieRating(MovieId,PersonId,Rating) VALUES(6,7,5);
+
+
+SELECT
+	m.MovieName,
+    p.FirstName,
+    p.LastName,
+    r.Rating 
 FROM PersonMovieRating r INNER JOIN RecentMovie m 
 on r.MovieId=m.MovieId
 INNER JOIN Person p
